8d225596a906c13474e2200dbc340cb7850e44ff
summersoul <17summersoul17@gmail.com>
2020-12-24 14:16:42 +0300

    END

--- include/cache.hpp
+++ include/cache.hpp
@@ -3,8 +3,6 @@
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
-#define NATURALS unsigned int //директива препроцессора сделана для удобства
-
 #include <vector>
 #include <cmath>
 #include <iostream>
@@ -29,26 +27,28 @@ public:
     void Straight_experiment();
     void Back_experiment();
     void Random_experiment();
-    static void inline Cache_warming(const NATURALS& cache_size, const double* arr1){
+    static void inline Cache_warming(const unsigned & cache_size, const double* arr1){
         [[maybe_unused]]double  k = 0;
-        for(NATURALS i = 0; i < cache_size; i+=16){
+        for(unsigned i = 0; i < cache_size; i+=16){
             k = arr1[i];
         }
     };
     const vector<double>& get_cache(){
         return cache;
     };
-    friend ostream& operator <<(ostream& os, Cache object);
+    friend ostream& operator <<(ostream& os, const Cache object);
 
 private:
     vector<double> cache;
     const vector<double> caches = {1, 4, 12};
     vector<Data> data_vector;
+    const int count_of_ints = 131072;
+    const int count_of_travels = 1000;
 };
 struct Data{
-    vector<NATURALS> time_vector;
+    vector<unsigned> time_vector;
     string travel;
-    Data(vector<NATURALS> time_vector_, string travel_)
+    Data(vector<unsigned> time_vector_, string travel_)
         : time_vector(time_vector_), travel(travel_){};
 };
 #endif // INCLUDE_HEADER_HPP_
--- sources/cache.cpp
+++ sources/cache.cpp
@@ -3,9 +3,6 @@
 #include <cache.hpp>
 #include <random>
 
-//512Kb<1Mb<2Mb<4Mb<8Mb<16Mb<18Mb
-//vector<int> cache = {512, 1024, 2048, 5096, 10192};
-
 void Cache::Cache_back(){
     cache.push_back(caches[0]*0.5);
     double n = 1;
@@ -17,59 +14,59 @@ void Cache::Cache_back(){
 }
 
 void Cache::Straight_experiment() {
-    vector<NATURALS> straight_time;
+    vector<unsigned> straight_time;
     for(size_t j = 0; j < cache.size(); ++j){
-        NATURALS cache_size = cache[j]*1024*1024/8; //count of ints in all cache lines
+        unsigned cache_size = cache[j]*count_of_ints; //count of ints in all cache lines
         double* arr1 = new double[cache_size];
         [[maybe_unused]]double k;
         Cache::Cache_warming(cache_size, arr1);
         auto startTime = std::chrono::high_resolution_clock::now();
-        for(NATURALS i = 0; i < cache_size*1000; i+=16){
+        for(unsigned i = 0; i < cache_size*count_of_travels; i+=16){
             k = arr1[i%1000];
         }
         auto endTime = std::chrono::high_resolution_clock::now();
-        straight_time.push_back(static_cast<NATURALS>(std::chrono::nanoseconds((endTime - startTime) / 1000).count()));
+        straight_time.push_back(static_cast<unsigned>(std::chrono::nanoseconds((endTime - startTime) / 1000).count()));
         delete[] arr1;
     }
     data_vector.emplace_back(straight_time, "straight");
 }
 void Cache::Back_experiment() {
-    vector<NATURALS> back_time;
+    vector<unsigned> back_time;
     for(size_t j = 0; j < cache.size(); ++j){
-        NATURALS cache_size = cache[j]*1024*1024/8;
+        unsigned cache_size = cache[j]*count_of_ints;
         double* arr1 = new double[cache_size];
         [[maybe_unused]]double k;
         Cache::Cache_warming(cache_size, arr1);
         auto startTime = std::chrono::high_resolution_clock::now();
-        for(NATURALS i = cache_size*1000; i > 0; i-=16){
+        for(unsigned i = cache_size*count_of_travels; i > 0; i-=16){
             k = arr1[i%1000];
         }
         auto endTime = std::chrono::high_resolution_clock::now();
-        back_time.push_back(static_cast<NATURALS>(std::chrono::nanoseconds((endTime - startTime) / 1000).count()));
+        back_time.push_back(static_cast<unsigned>(std::chrono::nanoseconds((endTime - startTime) / 1000).count()));
         delete[] arr1;
     }
     data_vector.emplace_back(back_time, "back");
 }
 
 void Cache::Random_experiment() {
-    vector<NATURALS> random_time;
+    vector<unsigned> random_time;
     for(size_t j = 0; j < cache.size(); ++j){
-        vector<NATURALS> arr;
-        vector<NATURALS>::iterator start, end;
-        NATURALS cache_size = cache[j]*1024*1024/8;
+        vector<unsigned> arr;
+        vector<unsigned>::iterator start, end;
+        unsigned cache_size = cache[j]*count_of_ints;
         double* arr1 = new double[cache_size];
         [[maybe_unused]]double k;
-        for(NATURALS i = 0; i < cache_size; i+=16) arr.emplace_back(i);
+        for(unsigned i = 0; i < cache_size; i+=16) arr.emplace_back(i);
             start = arr.begin();
             end = arr.end();
             shuffle(start, end,std::mt19937(std::random_device()()));
         Cache::Cache_warming(cache_size, arr1);
         auto startTime = std::chrono::high_resolution_clock::now();
-        for(NATURALS i = 0; i < cache_size*1000/16; ++i){
+        for(unsigned i = 0; i < cache_size*count_of_travels/16; ++i){
             k = arr1[arr[i%1000]];
         }
         auto endTime = std::chrono::high_resolution_clock::now();
-        random_time.push_back(static_cast<NATURALS>(std::chrono::nanoseconds((endTime - startTime) / 1000).count()));
+        random_time.push_back(static_cast<unsigned>(std::chrono::nanoseconds((endTime - startTime) / 1000).count()));
         delete[] arr1;
     }
     data_vector.emplace_back(random_time, "random");
